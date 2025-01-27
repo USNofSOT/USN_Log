8f87debaab8f11f37781ba2b44f42341b167a82b
Randy Gulak <randy@randygulak.com>
2025-01-27 15:01:23 -0800

    added icon to header

--- src/App.tsx
+++ src/App.tsx
@@ -445,10 +445,13 @@ function App() {
   // -----------------------------------
   return (
     <div className="min-h-screen bg-[#1a1a1a] text-white">
-      <header className="bg-[#2a2a2a] p-4 text-center flex justify-between items-center">
-        <h1 className="text-3xl font-bold text-amber-500">
-          SoT USN Voyage Log Generator
-        </h1>
+      <header className="bg-[#2a2a2a] py-1 px-4 pr-8 text-center flex justify-between items-center">
+        <div className="flex items-center gap-2">
+          <img className="w-20 h-20" src="/USN_Log/USN_Logo.png" alt="" />
+          <h1 className="text-3xl font-bold text-amber-500">
+            SoT USN Voyage Log Generator
+          </h1>
+        </div>
         <button
           onClick={toggleModal}
           className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
