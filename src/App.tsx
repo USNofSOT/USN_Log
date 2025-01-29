a030ea94408dea7b0e2c7cb8f23989ad37cca810
Randy Gulak <randy@randygulak.com>
2025-01-28 19:04:51 -0800

    simpified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,18 @@ import { LogProvider } from "./context/LogContext";
 function App() {
   return (
     <LogProvider>
-      <AppContent />
-    </LogProvider>
-  );
-}
+      <div className="min-h-screen bg-[#1a1a1a] text-white">
+        <Header />
+        <SettingsModal />
+        <CopyModal />
 
-function AppContent() {
-  return (
-    <div className="min-h-screen bg-[#1a1a1a] text-white">
-      <Header />
-      <SettingsModal />
-      <CopyModal />
-
-      {/* Main Layout */}
-      <div className="container mx-auto p-4 flex gap-4">
-        <Editor />
-        <LogPreview />
+        {/* Main Layout */}
+        <div className="container mx-auto p-4 flex gap-4">
+          <Editor />
+          <LogPreview />
+        </div>
       </div>
-    </div>
+    </LogProvider>
   );
 }
 
